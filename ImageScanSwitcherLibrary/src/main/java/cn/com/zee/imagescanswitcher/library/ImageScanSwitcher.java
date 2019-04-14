2c74e98bcb5654f9f8f7b449f6075956fd8d4dc5
zeekey.zhao <Zeekey.zhao@connext.com.cn>
2019-04-14 16:21:01 +0800

    alpha commit

--- ImageScanSwitcherLibrary/src/main/java/cn/com/zee/imagescanswitcher/library/ImageScanSwitcher.java
+++ ImageScanSwitcherLibrary/src/main/java/cn/com/zee/imagescanswitcher/library/ImageScanSwitcher.java
@@ -8,7 +8,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Iterator;
@@ -23,7 +26,11 @@ import androidx.annotation.Nullable;
 public class ImageScanSwitcher extends View {
 
     private static final int CACHE_LIMIT = 10;
+    private static final int STEP = 10;
     private final int MAX_CACHE_PIC_NUM = 7;
+
+    private final Object statusLock = new Object();
+    private final Object loadLock = new Object();
     private int currentX = 0;
     private int screenWidth;
     private int screenHeight;
@@ -37,6 +44,10 @@ public class ImageScanSwitcher extends View {
     private ConcurrentHashMap<Integer, BitmapData> bitmapCache = new ConcurrentHashMap<>();
     private Handler mHandler;
     private UpdateRunnable mUpdateRunnable;
+    private Handler mAsyncHandler;
+    private HandlerThread thread;
+    private LoadBitmapRunnable mLoadBitmapRunnable;
+    private boolean isLoading = false;
 
 
     public ImageScanSwitcher(Context context) {
@@ -69,30 +80,50 @@ public class ImageScanSwitcher extends View {
         bitmapPaint.setAntiAlias(true);
     }
 
-    private void setData(int[] resIds){
+    public void setData(int[] resIds){
         this.resIds = resIds;
+        loadBitmap(0,true);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if(!isRunning){
-            start();
-        }else{
-            if(isPaused){
-                drawView(canvas);
+        synchronized (statusLock) {
+            if (!isRunning) {
+                start();
+            } else {
+                if (!isPaused) {
+                    drawView(canvas);
+                }
             }
         }
         super.onDraw(canvas);
     }
 
-    private void start() {
-        if(mHandler != null){
-            if(mUpdateRunnable != null){
-                mHandler.removeCallbacks(mUpdateRunnable);
-            }else{
-                mUpdateRunnable = new UpdateRunnable();
+    public void start() {
+        synchronized (statusLock){
+            if(mHandler != null){
+                if(mUpdateRunnable != null){
+                    mHandler.removeCallbacks(mUpdateRunnable);
+                }else{
+                    mUpdateRunnable = new UpdateRunnable();
+                }
+                isRunning = true;
+                mHandler.post(mUpdateRunnable);
             }
-            mHandler.post(mUpdateRunnable);
+        }
+
+    }
+
+
+    public synchronized void resume() {
+        synchronized (statusLock) {
+            isPaused = false;
+        }
+    }
+
+    public synchronized void pause() {
+        synchronized (statusLock) {
+            isPaused = true;
         }
     }
 
@@ -103,28 +134,61 @@ public class ImageScanSwitcher extends View {
             if(mHandler != null){
                 mHandler.removeCallbacks(this);
             }
-            //TODO
+            synchronized (statusLock) {
+                if (!isPaused) {
+                    currentX+=STEP;
+                    if(currentX >= screenWidth){
+                        currentX = 0;
+                        firstVisibleIndex = nextIndex(firstVisibleIndex);
+                    }
+                    postInvalidate();
+                    if (mHandler != null) {
+                        if (mUpdateRunnable != null) {
+                            mHandler.postDelayed(mUpdateRunnable, 10L);
+                        }
+                    }
+                } else {
+                    if (mHandler != null) {
+                        if (mUpdateRunnable != null) {
+                            mHandler.postDelayed(mUpdateRunnable, 200L);
+                        }
+                    }
+                }
+            }
         }
     }
 
     private void drawView(Canvas canvas){
-        drawLeft(canvas);
-        drawLine(canvas);
-        drawRight(canvas);
+        if(canvas != null) {
+            drawLeft(canvas);
+            drawLine(canvas);
+            drawRight(canvas);
+        }
     }
 
     private void drawLeft(Canvas canvas) {
         if(currentX == 0){
             return;
         }
-        int leftIndex = firstVisibleIndex + 1;
+        int leftIndex = nextIndex(firstVisibleIndex);
+
         if(!bitmapCache.containsKey(leftIndex)){
-            loadBitmap(leftIndex);
+            synchronized (loadLock){
+                loadBitmap(leftIndex, true);
+            }
         }else{
             updateLastUsedTime(leftIndex);
         }
+        if(!bitmapCache.containsKey(nextIndex(leftIndex))){
+            loadBitmap(nextIndex(leftIndex), false);
+        }
         Rect drawSrcRect = new Rect(screenWidth - currentX,0,screenWidth,screenHeight);
-        Rect drawDstRect = new Rect(0,0,currentX,screenHeight);
+        Rect drawDstRect = new Rect();
+        getGlobalVisibleRect(drawDstRect);
+        drawDstRect.right = drawDstRect.left + currentX;
+        int height = drawDstRect.height();
+        drawDstRect.top = 0;
+        drawDstRect.bottom = height;
         canvas.drawBitmap(bitmapCache.get(leftIndex).getBitmap(),drawSrcRect, drawDstRect,bitmapPaint);
     }
 
@@ -138,27 +202,110 @@ public class ImageScanSwitcher extends View {
         }
         int rightIndex = firstVisibleIndex;
         if(!bitmapCache.containsKey(rightIndex)){
-            loadBitmap(rightIndex);
+            synchronized (loadLock){
+                loadBitmap(rightIndex, true);
+            }
         }else{
             updateLastUsedTime(rightIndex);
         }
+        if(!bitmapCache.containsKey(nextIndex(rightIndex))){
+            loadBitmap(nextIndex(rightIndex), false);
+        }
         Rect drawSrcRect = new Rect(0,0,screenWidth - currentX,screenHeight);
-        Rect drawDstRect = new Rect(currentX,0,screenWidth,screenHeight);
+        Rect drawDstRect = new Rect();
+        getGlobalVisibleRect(drawDstRect);
+        drawDstRect.left = currentX + drawDstRect.left;
+        int height = drawDstRect.height();
+        drawDstRect.top = 0;
+        drawDstRect.bottom = height;
         canvas.drawBitmap(bitmapCache.get(rightIndex).getBitmap(),drawSrcRect, drawDstRect,bitmapPaint);
     }
+
+
+
+    private int nextIndex(int index) {
+        if(index >= resIds.length - 1){
+            index = 0;
+        }else{
+            index ++;
+        }
+        return index;
+    }
+
+    private int previousIndex(int index) {
+        if(index <= 0){
+            index = resIds.length - 1;
+        }else{
+            index --;
+        }
+        return index;
+    }
+
+
+
+
 
     //cache start
     private void updateLastUsedTime(int index) {
         bitmapCache.get(index).setLastUseTimestamp(System.currentTimeMillis());
     }
 
-    private void loadBitmap(int index) {
+    private void loadBitmap(int index, boolean isSync) {
+
+        if(isSync){
+            Log.i("zzz", "zzz load :" + index + " isSync:" + isSync);
+            loadBitmapExe(index);
+        }else{
+            if(!isLoading){
+                Log.i("zzz", "zzz load :" + index + " isSync:" + isSync);
+                isLoading = true;
+                if(mAsyncHandler == null){
+                    initAsyncHandler();
+                }
+                if(mAsyncHandler != null){
+                    if(mLoadBitmapRunnable != null){
+                        mAsyncHandler.removeCallbacks(mLoadBitmapRunnable);
+                    }else{
+                        mLoadBitmapRunnable = new LoadBitmapRunnable();
+                    }
+                    mLoadBitmapRunnable.setIndex(index);
+                    mAsyncHandler.post(mLoadBitmapRunnable);
+                }
+            }
+
+        }
+    }
+
+    private class LoadBitmapRunnable implements Runnable{
+
+        private int index;
+
+        @Override
+        public void run() {
+            if(mAsyncHandler != null){
+                mAsyncHandler.removeCallbacks(this);
+            }
+            synchronized (loadLock) {
+                loadBitmapExe(index);
+            }
+            isLoading = false;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+
+    private void loadBitmapExe(int index){
         if(bitmapCache.size() > CACHE_LIMIT){
             dropOldestCache();
         }else{
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resIds[index]);
-            bitmapCache.put(index, new BitmapData(System.currentTimeMillis(), bitmap));
+            if(bitmap != null){
+                bitmapCache.put(index, new BitmapData(System.currentTimeMillis(), bitmap));
+            }
         }
+
     }
 
     private void dropOldestCache() {
@@ -187,6 +334,12 @@ public class ImageScanSwitcher extends View {
             }
             bitmapCache.remove(oldestKey);
         }
+    }
+
+    private void initAsyncHandler() {
+        thread = new HandlerThread("loadBitmap");
+        thread.start();
+        mAsyncHandler = new Handler(thread.getLooper());
     }
     //cache end
 
--- app/src/main/java/cn/com/zee/imagescanswitcher/demo/MainActivity.java
+++ app/src/main/java/cn/com/zee/imagescanswitcher/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.com.zee.imagescanswitcher.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import cn.com.zee.imagescanswitcher.library.ImageScanSwitcher;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageScanSwitcher imageScanSwitcher = findViewById(R.id.switcher);
+        imageScanSwitcher.setData(new int[]{R.mipmap.pic0,R.mipmap.pic1,R.mipmap.pic2,R.mipmap.pic3});
+        imageScanSwitcher.start();
     }
 }
